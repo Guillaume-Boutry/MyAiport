a484c952603963576a2ed8e521ed19cfcae22af3
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-02-24 10:22:55 +0100

    TP2 commit

---
+++ MyAirport.EF/Ressource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBO.MyAiport.EF
+{
+    class Ressource
+    {
+    }
+}
---
+++ MyAirport.EF/Vol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GBO.MyAiport.EF
+{
+    public class Vol
+    {
+        [Key]
+        public int VolID { get; set; }
+        public string Cie { get; set; }
+        public string Lig { get; set; }
+        public DateTime Dhc { get; set; }
+        public string Pkg { get; set; }
+        public string Imm { get; set; }
+        public short Pax { get; set; }
+        public string Des { get; set; }
+    }
+}
