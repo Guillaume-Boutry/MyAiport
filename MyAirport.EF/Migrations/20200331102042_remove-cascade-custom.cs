1203da39ee5ee1e41589e03c0fee9084f1913966
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-31 13:40:32 +0200

    Api Rest avec include de bagages

---
+++ MyAirport.EF/Migrations/20200331102042_remove-cascade-custom.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GBO.MyAiport.EF.Migrations
+{
+    public partial class removecascadecustom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bagages_Vols_VolID",
+                table: "Bagages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bagages_Vols_VolID",
+                table: "Bagages",
+                column: "VolID",
+                principalTable: "Vols",
+                principalColumn: "VolID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bagages_Vols_VolID",
+                table: "Bagages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bagages_Vols_VolID",
+                table: "Bagages",
+                column: "VolID",
+                principalTable: "Vols",
+                principalColumn: "VolID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
