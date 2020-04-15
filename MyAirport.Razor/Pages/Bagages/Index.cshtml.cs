45fbcbc8b4bd135ca9ecc45f6fdd14b5c7cf76a5
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-04-15 10:34:34 +0200

    Clean Controllers

---
+++ MyAirport.Razor/Pages/Bagages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GBO.MyAiport.EF;
+
+namespace GBO.MyAirport.Razor.Bagages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GBO.MyAiport.EF.MyAirportContext _context;
+
+        public IndexModel(GBO.MyAiport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Bagage> Bagage { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Bagage = await _context.Bagages.ToListAsync();
+        }
+    }
+}
---
+++ MyAirport.Razor/Pages/Vols/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GBO.MyAiport.EF;
+
+namespace GBO.MyAirport.Razor.Vols
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GBO.MyAiport.EF.MyAirportContext _context;
+
+        public IndexModel(GBO.MyAiport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Vol> Vol { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Vol = await _context.Vols.ToListAsync();
+        }
+    }
+}
