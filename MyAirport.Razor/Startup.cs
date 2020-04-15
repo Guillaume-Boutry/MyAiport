be20530c53fda12a37ac96db655f237ada8991d5
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-04-15 09:51:42 +0200

    Added Model Crud pages

--- MyAirport.Razor/Startup.cs
+++ MyAirport.Razor/Startup.cs
@@ -1,5 +1,7 @@
+using GBO.MyAiport.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace GBO.MyAirport.Razor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<MyAirportContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AirportContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
