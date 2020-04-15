90564137bae8dc3bb7506efe68da464ef28e22b6
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-04-15 09:24:21 +0200

    Add project to SLN, Change Root Namespace

--- MyAirport.Razor/Startup.cs
+++ MyAirport.Razor/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace MyAirport.Razor
+namespace GBO.MyAirport.Razor
 {
     public class Startup
     {
@@ -42,7 +37,6 @@ namespace MyAirport.Razor
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthorization();
