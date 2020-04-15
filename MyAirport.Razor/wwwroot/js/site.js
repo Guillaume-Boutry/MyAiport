7ef93f20ad479db6d4afcc873e3e9dceddab6ab0
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-04-15 16:22:09 +0200

    Search Field, moved scripts

--- MyAirport.Razor/wwwroot/js/site.js
+++ MyAirport.Razor/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$("#SearchString").keyup(function () {
+    const searchString = $(this).val().toLowerCase();
+    $("table tr").each(function (results) {
+        if (results !== 0) {
+            const text = $(this).find("td:nth-child(1)").text().toLowerCase();
+            if(text.includes(searchString)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        }
+    });
+});
